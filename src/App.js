1208e87d5c167d770b69d3aa30dac9e98a2058a7
RohitcoolKashyap <rohit.cse18@chitkarauniversity.edu.in>
2020-09-12 19:14:14 +0530

    add signup

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import "./App.scss";
 import Header from "./components/header/Header";
 import Main from "./components/mainContent/Main";
 import Footer from "./components/Footer/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SignUp from "./components/SignUp/SignUp";
 function App() {
   return (
     <div className="App">
-     
-      <Header />
-      <Main />
-      <Footer/>
-
-	  <Footer/>
-	  
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Main />
+            <Footer />
+          </Route>
+          <Route path="/signup" exact component={SignUp}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,36 +1,37 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 
 export default function Header() {
-
   return (
-      <div className="header">
-        {/* header left*/}
+    <div className="header">
+      {/* header left*/}
 
-        <div className="header__left">
+      <div className="header__left">
+        <Link to="/">
           <div className="header__logo">logo</div>
+        </Link>
 
-          <ul className="header__left__nav">
-            <li className="header__left__nav__list">Services</li>
-            <li className="header__left__nav__list">About</li>
+        <ul className="header__left__nav">
+          <li className="header__left__nav__list">Services</li>
+          <li className="header__left__nav__list">About</li>
 
-            <li className="header__left__nav__list">Community</li>
+          <li className="header__left__nav__list">Community</li>
 
-            <li className="header__left__nav__list">Docs</li>
-          </ul>
-        </div>
-
-        {/* header right*/}
-
-        <div className="header__right">
-          <ul className="header__right__nav">
-            <li className="header__right__nav__list">Contact</li>
-            <li className="header__right__nav__list">Sign In</li>
-            <>
-              <li className="header__right__nav__list">Register</li>
-            </>
-          </ul>
-        </div>
+          <li className="header__left__nav__list">Docs</li>
+        </ul>
       </div>
-  )
+
+      {/* header right*/}
+
+      <div className="header__right">
+        <ul className="header__right__nav">
+          <li className="header__right__nav__list">Contact</li>
+          <li className="header__right__nav__list">Sign In</li>
+          <li className="header__right__nav__list">
+            <Link to="/signup">Register</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
