c9f3cf2ff8bab35fed85ae31f640ad167720e25b
Neel9784 <neel9784@gmail.com>
2020-09-12 14:57:22 +0530

    signupform

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.scss";
 import Header from "./components/header/Header";
 import Main from "./components/mainContent/Main";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-	<h1>Nello Nihal</h1>
+     
       <Header />
       <Main />
+      <Footer/>
     </div>
   );
 }
