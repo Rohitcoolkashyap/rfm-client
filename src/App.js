c5dbd4779ff7ec02d30efa95f7ef1009860807ed
Neel9784 <neel9784@gmail.com>
2020-09-13 09:15:42 +0530

    nihal5

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Main from "./components/mainContent/Main";
 import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignUp from "./components/SignUp/SignUp";
+import SignIn from "./components/SignIn/SignIn";
 function App() {
   return (
     <div className="App">
@@ -16,6 +17,7 @@ function App() {
             <Footer />
           </Route>
           <Route path="/signup" exact component={SignUp}></Route>
+          <Route path="/signip" exact component={SignIn}></Route>
         </Switch>
       </Router>
     </div>
