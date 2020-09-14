b8fb3154e41f6c8fb3ee40e6d881c6a8c30e8b5c
RohitcoolKashyap <rohit.cse18@chitkarauniversity.edu.in>
2020-09-14 22:37:25 +0530

    authentication

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://e-commerce-website.vercel.app/api/products"
+    ? "https://rfm-backend.herokuapp.com/"
     : "http://localhost:8000";
 
 export default baseUrl;
