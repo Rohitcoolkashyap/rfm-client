46d72740d37b12c74127ca1b1b5de8355e8702ac
RohitcoolKashyap <rohit.cse18@chitkarauniversity.edu.in>
2020-09-17 14:34:00 +0530

    d

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://rfm-backend.herokuapp.com"
+    ? "https://rfmapi.herokuapp.com"
     : "http://localhost:8000";
 
 export default baseUrl;
